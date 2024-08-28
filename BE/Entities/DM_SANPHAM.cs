b9970dc0ee32448ccead521e7f73bc0c76493c7e
hoangnhii211203 <hoangnhii211203>
2024-08-28 21:31:10 +0700

    Add project files.

---
+++ BE/Entities/DM_SANPHAM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE.Entities;
+
+public partial class DM_SANPHAM
+{
+    public Guid Id { get; set; }
+
+    public Guid ThuongHieuId { get; set; }
+
+    public Guid TheLoaiId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public string? Decription { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public virtual DM_THELOAI TheLoai { get; set; } = null!;
+
+    public virtual DM_THUONGHIEU ThuongHieu { get; set; } = null!;
+}
---
+++ BE/Entities/DM_THUONGHIEU.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE.Entities;
+
+public partial class DM_THUONGHIEU
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+
+    public DateTime NgayTao { get; set; }
+
+    public string NguoiTao { get; set; } = null!;
+
+    public DateTime NgaySua { get; set; }
+
+    public string NguoiSua { get; set; } = null!;
+
+    public DateTime? NgayXoa { get; set; }
+
+    public string? NguoiXoa { get; set; }
+
+    public bool IsActived { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public virtual ICollection<DM_SANPHAM> DM_SANPHAMs { get; set; } = new List<DM_SANPHAM>();
+}
