2d648df8817143b8654806a6acbf90cd3892dd22
hoangnhii211203 <hoangnhii211203>
2024-09-29 16:37:09 +0700

    Hoàng Nhi - Thêm controller Test Encrypt - Decrypt

---
+++ BE/Controllers/HETHONG/EncryptDecryptController.cs
@@ -0,0 +1,59 @@
+﻿using BE.Helper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MODELS.COMMON;
+using MODELS.HETHONG.TAIKHOAN.Requests;
+
+namespace BE.Controllers.HETHONG
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class EncryptDecryptController : BaseController<EncryptDecryptController>
+    {
+        IConfiguration _config;
+        public EncryptDecryptController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Encrypt(string request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Ok(new ApiResponse(false, StatusCodes.Status400BadRequest, CommonFunc.GetModelStateAPI(ModelState)));
+                }
+                var result = Encrypt_Decrypt.Encrypt(request, _config);
+
+                return Ok(new ApiOkResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ApiResponse(false, StatusCodes.Status500InternalServerError, ex.Message));
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Decrypt(string request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Ok(new ApiResponse(false, StatusCodes.Status400BadRequest, CommonFunc.GetModelStateAPI(ModelState)));
+                }
+                var result = Encrypt_Decrypt.Decrypt(request, _config);
+
+                return Ok(new ApiOkResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ApiResponse(false, StatusCodes.Status500InternalServerError, ex.Message));
+            }
+        }
+    }
+}
