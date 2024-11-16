088fc5411bce829d63b96d8813b2226dccf73172
hoangnhii211203 <hoangnhii211203>
2024-11-16 08:21:42 +0700

    Hoàng Nhi - Hoàn thành giao diện thể loại

--- MODELS/COMMON/CommonConst.cs
+++ MODELS/COMMON/CommonConst.cs
@@ -3,5 +3,8 @@
     public static class CommonConst
     {
         public static int ExpireTime = 7; // Thời gian hết hạn chức thực là 7 ngày 
+
+        public static string[] _fileHinhAnhValid = new string[] { "jpg", "png", "jpeg" };
+
     }
 }
